343f0148de9a6ff2728e4665b96e48bac809dc6c
Carlos Andres Reyes Peña <carlosarp2000@gmail.com>
2022-02-11 00:23:35 -0500

    Django_Course

--- prmiosplatziapp/polls/tests.py
+++ prmiosplatziapp/polls/tests.py
@@ -1,3 +1,31 @@
+import datetime
+from django.utils import timezone
 from django.test import TestCase
 
-# Create your tests here.
+from .models import Question
+
+class QuestionModelTests(TestCase):
+
+
+    def test_was_published_recently_with_future_questions(self):
+        """Was_published_recently returns Flase for questios whose pub_date is in the future"""
+
+        time = timezone.now() + datetime.timedelta(days = 30)
+        future_question = Question(question_text="Quien es el mejor course director de platzi",pub_date=time)
+        self.assertIs(future_question.was_published_recently(), False)
+
+
+    def test_was_published_recently_with_past_questions(self):
+        """Was_published_recently returns Flase for questios whose pub_date is in the past"""
+
+        time = timezone.now() - datetime.timedelta(days = 30)
+        future_question = Question(question_text="Quien es el mejor course director de platzi",pub_date=time)
+        self.assertIs(future_question.was_published_recently(), False)
+
+    
+    def test_was_published_recently_with_today_questions(self):
+        """Was_published_recently returns Flase for questios whose pub_date is test_was_published_recently_with_today_questions"""
+
+        time = timezone.now()
+        future_question = Question(question_text="Quien es el mejor course director de platzi",pub_date=time)
+        self.assertIs(future_question.was_published_recently(), True)
--- prmiosplatziapp/prmiosplatziapp/settings.py
+++ prmiosplatziapp/prmiosplatziapp/settings.py
@@ -21,6 +21,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    "polls.apps.PollsConfig",
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -95,7 +96,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Bogota'
 
 USE_I18N = True
 
